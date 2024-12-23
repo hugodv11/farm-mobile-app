6936d3e5001fe7d0d807a4b19110af2da5eb67bc
Hugo Dalmeida Vázquez <hugodalmeidavazquez99@gmail.com>
2024-12-23 13:48:10 +0100

    ADD - Lint and formatter

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'react-native/react-native': true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-native/all',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-native',
+    '@typescript-eslint',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+    'eol-last': [1, 'always'],
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/no-unresolved': 'off',
+  },
+};
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
+import { ParamListBase, RouteProp } from '@react-navigation/native';
 import MapScreen from './(map)';
 import CropsScreen from './(crops)';
 
 const Tab = createBottomTabNavigator();
+
+const getTabIcon = (route: RouteProp<ParamListBase, string>) => {
+  switch (route.name) {
+    case 'map':
+      return <Ionicons name="map" size={24} />;
+
+    case 'crops':
+      return <Ionicons name="leaf" size={24} />;
+
+    default:
+      return '';
+  }
+};
 
 export default function TabLayout() {
   return (
     <Tab.Navigator
       initialRouteName="crops"
       screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
-
-          if (route.name === 'map') {
-            iconName = 'map';
-          } else if (route.name === 'crops') {
-            iconName = 'leaf';
-          }
-
-          return <Ionicons name={iconName as any} size={size} color={color} />;
-        },
+        tabBarIcon: () => getTabIcon(route),
       })}
     >
       <Tab.Screen name="map" component={MapScreen} options={{ headerShown: false }} />
