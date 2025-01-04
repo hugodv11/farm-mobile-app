268ea54f561395b7acac21150deefc476846be08
Hugo Dalmeida Vázquez <hugodalmeidavazquez99@gmail.com>
2025-01-04 19:30:06 +0100

    ADD - Basic database implementation

--- app/(tabs)/(crops)/index.tsx
+++ app/(tabs)/(crops)/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from 'react-native';
+import { getCrops, getDBConnection } from '@/app/services/db-service';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
 
 export default function Crops() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Crop list</Text>
-    </View>
-  );
+    const [loading, setLoading] = useState(true);
+    const [ crops, setCrops ] = useState([]);
+  
+    useEffect(() => {
+      const loadCrops = async () => {
+        const db = await getDBConnection();
+        const cropsResult = await getCrops(db);
+
+        console.log(cropsResult);
+
+        setLoading(false);
+
+        return cropsResult;
+      };
+  
+      setCrops(loadCrops());
+    }, []);
+
+
+   if (loading) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    } else {
+      return (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text>Crop list</Text>
+          <Text>{JSON.stringify(crops)}</Text>
+        </View>
+      );
+    }
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,34 @@
 import { Stack } from 'expo-router';
+import { ActivityIndicator, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { getDBConnection, createDefaultTables, defaultInserts } from './services/db-service';
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const initializeDatabase = async () => {
+      const db = await getDBConnection();
+      await createDefaultTables(db);
+      await defaultInserts(db);
+      setLoading(false);
+    };
+
+    initializeDatabase();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  } else {
+    return (
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    );
+  }
+
 }
