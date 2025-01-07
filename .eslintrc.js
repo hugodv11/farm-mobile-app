89f2fb24b6d8b74291e6560574d01db7745cf057
Hugo Dalmeida Vázquez <hugodalmeidavazquez99@gmail.com>
2025-01-07 10:41:08 +0100

    FIX - Linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    'react-native/react-native': true,
-  },
-  extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-native/all',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    'react-native',
-    '@typescript-eslint',
-  ],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
-    'eol-last': [1, 'always'],
-    'react/react-in-jsx-scope': 'off',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'import/no-unresolved': 'off',
+    "prettier/prettier": "error",
+    "eol-last": "error",
   },
 };
