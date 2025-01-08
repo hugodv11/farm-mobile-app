02bacbd01c3fa6ac29a4dc6ff7584136f874b83f
Hugo Dalmeida Vázquez <hugodalmeidavazquez99@gmail.com>
2025-01-08 12:42:59 +0100

    FIXES

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,9 @@
 import { Stack } from "expo-router";
-import { ActivityIndicator, View } from "react-native";
-import { useEffect, useState } from "react";
-import {
-  getDBConnection,
-  createDefaultTables,
-  defaultInserts,
-} from "./services/db-service";
 
 export default function RootLayout() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const initializeDatabase = async () => {
-      const db = await getDBConnection();
-      await createDefaultTables(db);
-      await defaultInserts(db);
-      setLoading(false);
-    };
-
-    initializeDatabase();
-  }, []);
-
-  if (loading) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  } else {
-    return (
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    );
-  }
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
